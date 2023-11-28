a2afd38ca5fce114184b4a42319f062deb638a09
Henry Merrilees <henry@merrile.es>
2023-11-28 14:01:49 -0800

    cleaned up python

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #python3 py/image_gen.py
-python3 image_loader.py
+python3 main.py load "mp.png" "input.mem"
 verilator --cc --timing --exe --build -j 0 -Wall --top-module color sim_main.cpp color.sv --trace
 obj_dir/Vcolor
-python3 image_view.py
+python3 main.py view_color "output.mem"
